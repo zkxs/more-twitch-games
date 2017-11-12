103aea21833c94f8ca8d32cdfe344e885e296424
Michael Ripley <zkxs00@gmail.com>
2017-11-12 04:05:55 -0600

    This would add support for go.twitch.tv... if the api I need existed
    Apparently you cannot request perms for multiple domains.  This forces me to request perms for "*://*.twitch.tv/*".  Also apparently neither Chrome nor FF webextensions support modifying request bodies.

--- src/more-twitch-games.js
+++ src/more-twitch-games.js
@@ -1,13 +1,27 @@
-console.log("sup bro");
+// legacy requests look like: https://api.twitch.tv/api/users/username/follows/games/live?on_site=1
+// new requests look like: https://gql.twitch.tv/gql
+const legacyUrlRegex = new RegExp("^https?://api\\.twitch\\.tv/api/users/[^/?]+/follows/games/live\\?on_site=1$");
+const legacyMatchPattern = "*://api.twitch.tv/api/users/*/follows/games/*";
+const newMatchPattern = "*://gql.twitch.tv/gql";
 
-// requests look like: https://api.twitch.tv/api/users/username/follows/games/live?on_site=1
-const regex = new RegExp("^https?://api\\.twitch\\.tv/api/users/[^/?]+/follows/games/live\\?on_site=1$");
-const matchPattern = "*://api.twitch.tv/api/users/*/follows/games/*"; // same as the pattern in the manifest
+function arrayBufferToString(buffer) {
+  return String.fromCharCode.apply(null, new Uint8Array(buffer));
+}
 
-function listener(requestDetails) {
+function stringToArrayBuffer(string) {
+  const stringLength = string.length;
+  const buffer = new ArrayBuffer(stringLength);
+  const bufferView = new Uint8Array(buffer);
+  for (let i = 0; i < stringLength; i++) {
+    bufferView[i] = string.charCodeAt(i);
+  }
+  return buffer;
+}
+
+function legacyRequestListener(requestDetails) {
   
   // make sure the url actually matches
-  if (regex.test(requestDetails.url)) {
+  if (legacyUrlRegex.test(requestDetails.url)) {
     const modifiedUrl = requestDetails.url + "&limit=200";
     console.log("rewriting", requestDetails.url, "to", modifiedUrl);
     return {redirectUrl: modifiedUrl};
@@ -16,4 +30,38 @@ function listener(requestDetails) {
   }
 }
 
-browser.webRequest.onBeforeRequest.addListener(listener, {urls: [matchPattern]}, ["blocking"]);
+function newRequestListener(requestDetails) {
+  
+  if (requestDetails.method === "POST" && requestDetails.requestBody) {
+    const buffer = requestDetails.requestBody.raw[0].bytes;
+    const string = arrayBufferToString(buffer);
+    
+    try {
+      const object = JSON.parse(string);
+      let modified = false;
+      for (const query of object) {
+        if (query.operationName === "FollowingGames_CurrentUser") {
+          query.variables.limit = 200;
+          modified = true;
+        }
+      }
+      
+      if (modified) {
+        console.log("Patching GQL request", requestDetails.method, requestDetails.url, object, "initiated by", requestDetails.originUrl);
+        requestDetails.requestBody.raw[0].bytes = stringToArrayBuffer(JSON.stringify(object));
+      }
+      
+    } catch (error) {
+      console.error(error, error.stack);
+      console.error(string);
+    }
+    
+  }
+  
+  return {};
+}
+
+browser.webRequest.onBeforeRequest.addListener(legacyRequestListener, {urls: [legacyMatchPattern]}, ["blocking"]);
+browser.webRequest.onBeforeRequest.addListener(newRequestListener, {urls: [newMatchPattern]}, ["blocking", "requestBody"]);
+
+console.log("Finished loading more-twitch-games.js");
